d95821d4a7c02d8fe03b407a72540cb7e5cd48e9
polismai <polismai@gmail.com>
2025-03-13 19:11:27 -0300

    Wrap topbar component

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -2,36 +2,39 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const TopBar = () => {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link href="#">Telefono</Link>
-        </li>
-        <li>
-          <Link href="#">Ubicacion</Link>
-        </li>
-        <li>
-          <Link href="#">Soporte</Link>
-        </li>
-      </ul>
+  const contactLinks = [
+    { href: "#", icon: "/ico-small-phone.svg", text: "Teléfono" },
+    { href: "#", icon: "/ico-small-location.svg", text: "Ubicación" },
+    { href: "#", icon: "/ico-small-clock.svg", text: "Soporte" },
+  ];
 
-      <ul>
-        <li>
-          <Link href="#"><Image src="/ico-small-fb.svg" width={24} height={24} alt="" /></Link>
-        </li>
-        <li>
-          <Link href="#"><Image src="/ico-small-ig.svg" width={24} height={24} alt="" /></Link>
-        </li>
-        <li>
-          <Link href="#"><Image src="/ico-small-location.svg" width={24} height={24} alt="" /></Link>
-        </li>
-        <li>
-          <Link href="#"><Image src="/ico-small-clock.svg" width={24} height={24} alt="" /></Link>
-        </li>
-      </ul>
+  const socialLinks = [
+    { href: "#", icon: "/ico-small-fb.svg", alt: "Facebook" },
+    { href: "#", icon: "/ico-small-tw.svg", alt: "Twitter" },
+    { href: "#", icon: "/ico-small-ig.svg", alt: "Instagram" },
+    { href: "#", icon: "/ico-small-pin.svg", alt: "Pinterest" },
+  ];
+
+  return (
+    <div className="flex bg-black w-full p-2 text-white justify-between text-xs">
+      <div className="flex gap-6">
+        {contactLinks.map(({ href, icon, text }) => (
+          <Link key={text} href={href} className="flex items-center gap-2">
+            <Image src={icon} width={24} height={24} alt={text} />
+            <span>{text}</span>
+          </Link>
+        ))}
+      </div>
+
+      <div className="flex gap-4">
+        {socialLinks.map(({ href, icon, alt }) => (
+          <Link key={alt} href={href} aria-label={alt}>
+            <Image src={icon} width={24} height={24} alt={alt} />
+          </Link>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TopBar;
