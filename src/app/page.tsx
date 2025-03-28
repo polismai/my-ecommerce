b543963195fc0c4de848550d1cd0ebfe1899e6a6
polismai <polismai@gmail.com>
2025-03-28 19:02:21 -0300

    include images in Subscribe section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,11 @@ import Button from "../components/button/Button";
 import { groupProductsByCategory } from "@/utils/groupProductsByCategory";
 
 //images
+import Image from "next/image";
 import bannerSale from "/public/banner-sale.jpg";
 import bannerNewSeason from "/public/hero-banner.jpg";
+import womenStanding from "/public/woman-removebg-preview.png";
+import menWalking from "/public/men-removebg-preview.png";
 
 
 export type Product = {
@@ -88,8 +91,24 @@ export default async function Home() {
         </section> 
 
         <section className="w-full max-w-full">
-          <div className="bg-gradient-to-b from-[#F3F2F2] to-[#DCDBDB] my-8 p-6">
-            <article className="bg-white p-8 text-center max-w-[33rem] m-auto">
+          <div className="bg-gradient-to-b from-[#F3F2F2] to-[#DCDBDB] mt-[14.75rem] md:my-8 md:mx-auto p-6 md:p-[3.55rem] relative">
+            <div className="absolute w-32 md:w-[311px] h-[242px] md:h-[545px] top-[calc(-242px+1.5rem)] md:top-auto md:bottom-0 left-6 md:left-1/2 md:transform md:translate-x-[-530px]">
+              <Image 
+                src={womenStanding} 
+                alt="" 
+                fill
+                className="object-contain md:object-cover" 
+              />
+            </div>
+            <div className="absolute w-[99px] md:w-[219px] h-[236px] md:h-[524px] top-[calc(-236px+1.5rem)] md:top-auto md:bottom-0 right-8 md:right-1/2 md:transform md:translate-x-[470px]">
+              <Image 
+                src={menWalking} 
+                alt="" 
+                fill
+                className="object-contain md:object-cover" 
+              />
+            </div>
+            <article className="bg-white flex md:h-[28.75rem] p-8 text-center max-w-[33.25rem] m-auto">
               <div className="grid gap-8 max-w-[22rem] m-auto text-center">
                 <header>
                   <h3 className="text-sm uppercase text-gray-400">Special Offer</h3>
@@ -108,7 +127,7 @@ export default async function Home() {
                       className="text-center border border-gray-300 bg-gray-100 h-16"
                     />
                   </div>
-                  <Button>Subscribe</Button>
+                  <Button size="xl">Subscribe</Button>
                 </form>
               </div>
             </article>
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,17 +2,26 @@ import React from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary";
+  size?: "sm" | "md" | "lg" | "xl";
 }
 
-const Button: React.FC<ButtonProps> = ({ children, variant = "primary", className = "", ...props }) => {
-  const baseStyles = "h-16 uppercase text-white";
+const Button: React.FC<ButtonProps> = ({ children, variant = "primary", size = "md", className = "", ...props }) => {
+  const baseStyles = "uppercase px-6 hover:bg-gray-800";
+  
   const variants = {
-    primary: "bg-black hover:bg-gray-800",
+    primary: "bg-black text-white",
     secondary: "bg-gray-600 hover:bg-gray-700",
   };
 
+  const sizes = {
+    sm: "text-[0.6667rem]",
+    md: "text-[0.7222rem]",
+    lg: "text-[0.7222rem]",
+    xl: "text-[1rem] h-16",
+  };
+
   return (
-    <button className={`${baseStyles} ${variants[variant]} ${className}`} {...props}>
+    <button className={`${baseStyles} ${variants[variant]} ${sizes[size]} ${className}`} {...props}>
       {children}
     </button>
   );
